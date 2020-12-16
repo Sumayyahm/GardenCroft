a2c14961f0b45e1a0df016497dcd6ad83ce00bae
Sumayyah Mariam <sumayyahmariam019@gmail.com>
2020-12-16 17:37:25 -0600

    Created App.js with all the components and Routes to all components

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import Home from './components/Home'
+import Plants from "./components/Plants";
+import Annuals from './components/Annuals/Annuals';
+import Perennial from './components/Perennials/Perennials';
+import Roses from './components/Roses/Roses';
+import Footer from "./components/Footer";
+import { CartProvider } from "./CartContext";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Contact from './components/Contact';
+import Cart from './components/CartPage';
+import Stripe from './components/Stripe/StripeContainer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+class App extends Component {
+    render() {
+      return (  
+          <CartProvider>    
+         <BrowserRouter>
+          <div>
+            <Navbar />
+              <Switch>
+               <Route exact path={["/", "/home"]} >
+                   <Home />
+               </Route>
+               <Route path="/cart">
+                    <Cart />
+               </Route>
+               <Route path={`/plants/annuals`}>
+                    <Annuals />
+                </Route>
+                <Route path={`/plants/perennial`}>
+                    <Perennial />
+                </Route>
+                <Route path={`/plants/roses`}>
+                    <Roses />
+                </Route>
+               <Route exact path="/plants">
+                    <Plants />
+               </Route>
+               <Route exact path="/contact">
+                    <Contact />
+               </Route>
+               <Route exact path="/checkout">
+                   <Stripe />
+               </Route>
+              <Route component={Error}/>
+             </Switch>
+            <Footer /> 
+          </div> 
+        </BrowserRouter>
+        </CartProvider>
+      );
+    }
+  }
+   
+  export default App;
+
+    
+    
+  
+
+
