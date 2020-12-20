fcc08f6e82b7bf8d6a2b1f4c13b5ee4d8c8999aa
Sumayyah Mariam <sumayyahmariam019@gmail.com>
2020-12-19 22:20:43 -0600

    Removed post route for stripe and moved to routes folder

--- server.js
+++ server.js
@@ -22,43 +22,6 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors())
 app.use(routes);
 
-// Set your secret key. Remember to switch to your live secret key in production!
-// See your keys here: https://dashboard.stripe.com/account/apikeys
-// const stripe = require('stripe')();
-
-// const paymentIntent = await stripe.paymentIntents.create({
-//   amount: 1099,
-//   currency: 'usd',
-//   payment_method_types: ['card'],
-//   confirm: true
-// });
-
-app.post("/stripe/charge", cors(), async (req, res) => {
-  console.log("stripe-routes.js 9 | route reached", req.body);
-  let { amount, id } = req.body;
-  console.log("stripe-routes.js 10 | amount and id", amount, id);
-  try {
-    const payment = await stripe.paymentIntents.create({
-      amount: amount,
-      currency: "USD",
-      description: "Your Company Description",
-      payment_method: id,
-      confirm: true,
-    });
-    console.log("stripe-routes.js 19 | payment", payment);
-    res.json({
-      message: "Payment Successful",
-      success: true,
-    });
-  } catch (error) {
-    console.log("stripe-routes.js 17 | error", error);
-    res.json({
-      message: "Payment Failed",
-      success: false,
-    });
-  }
-});
-
 // Connect to the Mongo DB
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/plantsinfo",
