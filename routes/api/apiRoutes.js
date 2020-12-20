9baae5ff999c622fd6d8f5ebf95dfa48a4a9e118
Sumayyah Mariam <sumayyahmariam019@gmail.com>
2020-12-19 22:19:23 -0600

    Added routes for houseplants, succulents and stripe

--- routes/api/apiRoutes.js
+++ routes/api/apiRoutes.js
@@ -2,9 +2,12 @@ const router = require("express").Router();
 const db = require("../../models");
 const nodemailer= require('nodemailer');
 require("dotenv").config();
+const cors= require("cors");
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST)
 
-// Matches with "/api/plants"
-router.get("/plants", (req,res) => {
+// Matches with "/api/annual"
+router.get("/annual", (req,res) => {
     db.Plant
           .find({plant_type: "Annual"})
           .then(dbModel => res.json(dbModel))
@@ -25,6 +28,22 @@ router.get("/roses", (req, res) => {
             .find({plant_type: "Rose Plant"})
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
+})
+
+// Matches with "/api/houseplant"
+router.get("/houseplant", (req, res) => {
+  db.Plant
+          .find({plant_type: "House Plant"})
+          .then(dbModel => res.json(dbModel))
+          .catch(err => res.status(422).json(err));
+});
+
+// Matches with "/api/succulent"
+router.get("/succulent", (req, res) => {
+  db.Plant
+          .find({plant_type: "Succulent"})
+          .then(dbModel => res.json(dbModel))
+          .catch(err => res.status(422).json(err));
 })
 
 router.get("/form",() => {
@@ -73,6 +92,34 @@ router.post("/form", (req,res) => {
 
     smtpTransport.close();
 })
+
+router.post("/stripe/charge", cors(), async (req, res) => {
+    console.log("stripe-routes.js 9 | route reached", req.body);
+    let { amount, id, receipt_email } = req.body;
+    console.log("stripe-routes.js 10 | amount, id and email", amount, id, receipt_email);
+    try {
+      const payment = await stripe.paymentIntents.create({
+        amount,
+        receipt_email,
+        currency: "USD",
+        description: "Garden Croft Purchase",
+        payment_method: id,
+        confirm: true
+      });
+      console.log("stripe-routes.js 19 | payment", payment);
+      res.json({
+        message: "Payment Successful",
+        success: true,
+      });
+    } catch (error) {
+      console.log("stripe-routes.js 17 | error", error);
+      res.json({
+        message: "Payment Failed",
+        success: false,
+      });
+    }
+  });
+
 
 module.exports = router;
 
