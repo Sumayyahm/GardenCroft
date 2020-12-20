d770a58e44890b821caa3d518e06e7d43994480b
Sumayyah Mariam <sumayyahmariam019@gmail.com>
2020-12-19 22:36:37 -0600

    added Css styles

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,28 +1,33 @@
 import React from 'react';
 import { Menu, Dropdown, Image, Icon } from 'semantic-ui-react';
-import logo from "../images/logo.png";
+import greencroft from "../images/greencroft.png";
 import { Link } from "react-router-dom";
 
-// import Search from "./Search";
-import App from '../App';
+const style={
+    display: "block",
+    marginLeft: "85px"
+}
+
+const style1={
+  backgroundColor: 'whitesmoke',
+  padding: '1px'
+}
+
+const style2={
+  color:'yellowgreen',
+  fontSize:'23px',
+  fontFamily:'Palatino, serif'
+}
 
 
 const DropdownExampleMenuDirection = () => (
  <div>
   
-  <Menu attached='top'>
-  <Link to="/home">
-  <a><Image src={logo} size='medium'/></a>
-  </Link>
-    <Menu.Menu position='right'>
-      <a href="#">
-        <Link to="/cart">
-          <Icon color='green' name='cart' size='big' style={{marginTop:'50px', marginRight:'20px'}} />
-        </Link>
-      </a>
-      <Dropdown item text='Menu' icon='angle down' style={{fontSize:'30px'}} simple>
-          <Dropdown.Menu style={{fontSize:'30px', color: 'green'}}>
-            <Dropdown.Item>  
+  <Menu style={style1} attached='top'>
+    <Menu.Menu position='left'>
+        <Dropdown item text='Menu' icon='angle down' style={style2} simple>
+          <Dropdown.Menu>
+            <Dropdown.Item style={{color:'seagreen'}}>  
               <Link to="/plants">
                 Plants
               </Link>  
@@ -37,11 +42,6 @@ const DropdownExampleMenuDirection = () => (
                 Services
               </Link> 
             </Dropdown.Item>
-            {/* <Dropdown.Item>
-            <Link to="/cart">
-              Blog
-              </Link> 
-              </Dropdown.Item> */}
             <Dropdown.Item>
               <Link to="/contact">
               Contact Us
@@ -50,6 +50,20 @@ const DropdownExampleMenuDirection = () => (
           </Dropdown.Menu>
       </Dropdown>
     </Menu.Menu>
+    {/* <Link to="/home">
+  <a><Image 
+  src={greencroft} 
+  size='medium' 
+  style={style} 
+  position='center'
+  />
+  </a>
+  </Link> */}
+      <Menu.Menu position='right'>
+        <Link to="/cart" position='right'>
+          <Icon name='cart' size='big' style={{marginTop:'20px', marginRight:'20px', color: 'yellowgreen'}} />
+        </Link>
+      </Menu.Menu>
   </Menu>
   </div>
 )
